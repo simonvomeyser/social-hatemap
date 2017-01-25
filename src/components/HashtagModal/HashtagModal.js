1ce58d750a5021b8786ab81c06ff19ed53d71758
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-01-25 16:29:45 +0100

    Added react router, implemented example

---
+++ src/components/HashtagModal/HashtagModal.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HashtagModal extends Component {
+  render() {
+    return (
+      <div className="HashtagModal">
+        <form action="asodn">
+          Please enter your Hashtag
+          <input type="text"/>
+        </form>        
+      </div>
+    );
+  }
+}
+
+export default HashtagModal;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
+import HashtagModal from './components/HashtagModal/HashtagModal';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <div>
+    <Router history={hashHistory}>
+      <Route path="/">
+        <IndexRoute component={HashtagModal} />
+        <Route path="about" component={App} />
+      </Route>
+    </Router>
+  </div>
+  ), document.getElementById('root')
 );
