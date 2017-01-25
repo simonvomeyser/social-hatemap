5ee654fe0febf4adbcb4560be6c1dda6b24356b7
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-01-25 16:30:22 +0100

    added map as background to app

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import App from './components/App/App';
 import HashtagModal from './components/HashtagModal/HashtagModal';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 import './index.css';
+import svgMap from './svgMap.svg';
+
+var style = {
+  backgroundImage: 'url('+svgMap+')',
+  backgroundColor: '#222',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center, center',
+  height: '100vh'
+};
 
 ReactDOM.render((
-  <div>
+  <div style={style}>
     <Router history={hashHistory}>
       <Route path="/">
         <IndexRoute component={HashtagModal} />
