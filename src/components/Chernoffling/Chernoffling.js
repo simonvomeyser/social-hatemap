0126564f0479b11cdcdf79468c818af7376615aa
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-02 19:06:13 +0100

    Proof of concept: added svg manipulation lib to component

--- src/components/Chernoffling/Chernoffling.js
+++ src/components/Chernoffling/Chernoffling.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 import './Chernoffling.css';
+import SVG from 'svg.js';
 
 /**
- *  to see the impact every var has on the chernoffling
+ * Main Component responsible for drawing the chernoffling
  */
 export default class Chernoffling extends React.Component {
+  componentDidMount() {
+    var draw = SVG('Chernoffling__svg1').size('100%', '100%');
+    var rect = draw.circle('100%', '100%').attr({ fill: '#f06' });
+  }
   render() {
     return (
       <div className="Chernoffling">
-        <div className="Chernoffling__svg" ></div>
+        <div className="Chernoffling__svg" id="Chernoffling__svg1"></div>
       </div>
     );
   }
