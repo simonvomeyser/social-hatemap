990d719b1b80998b5cf93996e2326e385d4c4ee3
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-02 20:03:46 +0100

    wip: drawing a new svg on each update

--- src/components/Chernoffling/Chernoffling.js
+++ src/components/Chernoffling/Chernoffling.js
@@ -8,8 +8,27 @@ import SVG from 'svg.js';
  */
 export default class Chernoffling extends React.Component {
   componentDidMount() {
-    var draw = SVG('Chernoffling__svg1').size('100%', '100%');
-    var rect = draw.circle('100%', '100%').attr({ fill: '#f06' });
+    const chernoffling = {...this.drawBasicChernoffling()};
+    this.setState(chernoffling: chernoffling);
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log ("will recive props"); //Debug
+    const chernoffling = this.state.chernoffling;
+    console.log (chernoffling); //Debug
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log ("update"); //Debug
+
+    // var mouthLeft = draw.line('15%', '65%','50%',  '85%').stroke({ width: '5%' });
+    // var mouthRight = draw.line('50%', '85%','85%', '65%').stroke({ width: '5%' });
+    // this.setState({draw:draw});
+    // // Draw basic chernoffling
+    // console.log ("mount"); //Debug
+    // Mout depends on anger
+    // var mouthAltering = 50 - this.props.anger;
+  }
+  componentWillUnmount() {
+
   }
   render() {
     return (
@@ -17,5 +36,13 @@ export default class Chernoffling extends React.Component {
         <div className="Chernoffling__svg" id="Chernoffling__svg1"></div>
       </div>
     );
+  }
+  drawBasicChernoffling() {
+    const basicChernoffling = {};
+    basicChernoffling.canvas = SVG('Chernoffling__svg1').size('100%', '100%');
+    basicChernoffling.rect = basicChernoffling.canvas.circle('100%', '100%').attr({ fill: 'grey' });
+    basicChernoffling.eyeLeft = basicChernoffling.canvas.ellipse('30%', '20%').attr({ fill: '#343434', 'cx' : '25%', 'cy' : '35%' });
+    basicChernoffling.rightLeft = basicChernoffling.canvas.ellipse('30%', '20%').attr({ fill: '#343434', 'cx' : '75%', 'cy' : '35%' });    
+    return basicChernoffling;
   }
 }
