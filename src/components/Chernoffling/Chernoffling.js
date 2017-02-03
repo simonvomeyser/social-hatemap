4e86221f8eadb014ea073f1d4524aadfcd35df2e
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-03 14:56:21 +0100

    wip - example of svg changing inside of component

--- src/components/Chernoffling/Chernoffling.js
+++ src/components/Chernoffling/Chernoffling.js
@@ -7,28 +7,30 @@ import SVG from 'svg.js';
  * Main Component responsible for drawing the chernoffling
  */
 export default class Chernoffling extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+  }
   componentDidMount() {
-    const chernoffling = {...this.drawBasicChernoffling()};
-    this.setState(chernoffling: chernoffling);
+
+    const chernoffling = {...this.draw(this.props)};
+    this.setState({c: chernoffling}); 
   }
   componentWillReceiveProps(nextProps) {
-    console.log ("will recive props"); //Debug
-    const chernoffling = this.state.chernoffling;
-    console.log (chernoffling); //Debug
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log ("update"); //Debug
+    // Remove old chernoffling
+    this.removeChernoffling();
 
-    // var mouthLeft = draw.line('15%', '65%','50%',  '85%').stroke({ width: '5%' });
-    // var mouthRight = draw.line('50%', '85%','85%', '65%').stroke({ width: '5%' });
-    // this.setState({draw:draw});
-    // // Draw basic chernoffling
-    // console.log ("mount"); //Debug
-    // Mout depends on anger
-    // var mouthAltering = 50 - this.props.anger;
+    // Create a new one with new props
+    const newChernoffling = {...this.draw(nextProps)};
+    this.setState({c: newChernoffling}); 
+  }
+  removeChernoffling() {
+    this.state.c.canvas.remove();
+    this.setState({c : null});
   }
   componentWillUnmount() {
-
+    this.removeChernoffling();
   }
   render() {
     return (
@@ -37,12 +39,42 @@ export default class Chernoffling extends React.Component {
       </div>
     );
   }
-  drawBasicChernoffling() {
-    const basicChernoffling = {};
-    basicChernoffling.canvas = SVG('Chernoffling__svg1').size('100%', '100%');
-    basicChernoffling.rect = basicChernoffling.canvas.circle('100%', '100%').attr({ fill: 'grey' });
-    basicChernoffling.eyeLeft = basicChernoffling.canvas.ellipse('30%', '20%').attr({ fill: '#343434', 'cx' : '25%', 'cy' : '35%' });
-    basicChernoffling.rightLeft = basicChernoffling.canvas.ellipse('30%', '20%').attr({ fill: '#343434', 'cx' : '75%', 'cy' : '35%' });    
-    return basicChernoffling;
+  /**
+   * Main function used to draw the creature
+   * 
+   * Doc: https://svgdotjs.github.io/
+   */
+  draw(params) {
+
+    let size;
+
+    if (params.posts < 20) {
+      size = 20;
+    } else if(params.posts < 100) {
+      size = params.posts;
+    } else {
+      size = 100;
+    }
+
+    const c = {};
+    c.canvas = SVG('Chernoffling__svg1').size(size+'%', size+'%');
+    c.rect = c.canvas.circle('100%', '100%').attr({ fill: 'grey' });
+    c.eyeLeft = c.canvas.ellipse('15%', '15%').attr({ fill: '#343434', 'cx' : '25%', 'cy' : '35%' });
+    c.rightLeft = c.canvas.ellipse('15%', '15%').attr({ fill: '#343434', 'cx' : '75%', 'cy' : '35%' });    
+
+    // Mouth example:
+    const normalMouthPosition = 75; // where mouth is like "meh" meaning 50% anger
+    const defaultAngerLevel =  50; 
+    const differnceFactorToAngerLevel =  normalMouthPosition / defaultAngerLevel; 
+    const moveMouthBy = (params.anger * differnceFactorToAngerLevel - normalMouthPosition) / -7;
+    const mouthPosition = (normalMouthPosition + moveMouthBy);
+    c.rect.fill();
+    // With 50%, mouth position should be 65%
+    // With 75%
+
+    c.mouthLeft = c.canvas.line('15%', normalMouthPosition+'%','50%',  mouthPosition+'%').stroke({ width: '5%' });
+    c.mouthRight = c.canvas.line('50%', mouthPosition+'%','85%', normalMouthPosition+'%').stroke({ width: '5%' });
+
+    return c;
   }
 }
