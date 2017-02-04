a719975eba46e28e25b9b98f44ea9b9cdca72eeb
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-04 22:50:18 +0100

    Example of simple svg manipulation with jquery - cleaned up Component

--- src/components/Chernoffling/Chernoffling.js
+++ src/components/Chernoffling/Chernoffling.js
@@ -2,57 +2,41 @@ import React from 'react';
 
 import './Chernoffling.css';
 import monster from './monster.svg';
-import SVG from 'svg.js';
+import star from './star.svg';
+
+import $ from 'jquery';
 
 import SVGInline from "react-svg-inline";
 /**
  * Main Component responsible for drawing the chernoffling
  */
 export default class Chernoffling extends React.Component {
-  constructor(props) {
-    super(props);
-  
-    this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
-  }
   componentDidMount() {
 
-    const chernoffling = {...this.draw(this.props)};
-    this.setState({c: chernoffling}); 
+    this.draw(this.props);
   }
   componentWillReceiveProps(nextProps) {
-    // Remove old chernoffling
-    this.removeChernoffling();
 
-    // Create a new one with new props
-    const newChernoffling = {...this.draw(nextProps)};
-    this.setState({c: newChernoffling}); 
-  }
-  removeChernoffling() {
-    this.state.c.canvas.remove();
-    this.setState({c : null});
-  }
-  componentWillUnmount() {
-    this.removeChernoffling();
+    this.draw(this.props);
   }
   render() {
     return (
       <div className="Chernoffling">
-        <div className="Chernoffling__svg" id="Chernoffling__svg1">
+        <div className="Chernoffling__svg">
           <SVGInline svg={monster} />
+          <SVGInline svg={star} />
         </div>
       </div>
     );
   }
   /**
    * Main function used to draw the creature
-   * 
-   * Doc: https://svgdotjs.github.io/
    */
   draw(params) {
 
-    return "";
-    let size;
 
+    // Size controlles by number of posts
+    let size;
     if (params.posts < 20) {
       size = 20;
     } else if(params.posts < 100) {
@@ -60,26 +44,27 @@ export default class Chernoffling extends React.Component {
     } else {
       size = 100;
     }
+    $('#monster').css({
+      width: size+"%",
+      heigth: size+"%" 
+    });
 
-    const c = {};
-    c.canvas = SVG('Chernoffling__svg1').size(size+'%', size+'%');
-    c.rect = c.canvas.circle('100%', '100%').attr({ fill: 'grey' });
-    c.eyeLeft = c.canvas.ellipse('15%', '15%').attr({ fill: '#343434', 'cx' : '25%', 'cy' : '35%' });
-    c.rightLeft = c.canvas.ellipse('15%', '15%').attr({ fill: '#343434', 'cx' : '75%', 'cy' : '35%' });    
+    // star size controlled by percentage of all posts
+    const $star = $('#star').css({
+          width: params.percentage+"px",
+          heigth: params.percentage+"px" 
+        });
 
-    // Mouth example:
-    const normalMouthPosition = 75; // where mouth is like "meh" meaning 50% anger
-    const defaultAngerLevel =  50; 
-    const differnceFactorToAngerLevel =  normalMouthPosition / defaultAngerLevel; 
-    const moveMouthBy = (params.anger * differnceFactorToAngerLevel - normalMouthPosition) / -7;
-    const mouthPosition = (normalMouthPosition + moveMouthBy);
-    c.rect.fill();
-    // With 50%, mouth position should be 65%
-    // With 75%
+    // Color controlled by anger
+    if (params.anger > 50) {
+      $('.cls-3').css({fill: "#fd6137"});
+    }
+    else if (params.anger < 50) {
+      $('.cls-3').css({fill: "#18beff"});
 
-    c.mouthLeft = c.canvas.line('15%', normalMouthPosition+'%','50%',  mouthPosition+'%').stroke({ width: '5%' });
-    c.mouthRight = c.canvas.line('50%', mouthPosition+'%','85%', normalMouthPosition+'%').stroke({ width: '5%' });
+    } else {
+      $('.cls-3').css({fill: ""});
+    }
 
-    return c;
   }
 }
