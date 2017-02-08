e49c5bdf9afbf3a223ef4ce2bcf86d50912ded79
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-08 19:08:49 +0100

    wip - created geocoding api brigde class

---
+++ src/apis/geocoding/Geocoder.js
@@ -0,0 +1,14 @@
+const apiUrl = 'http://api.socialhatemap.com/index.php';
+
+const Geocoder = {
+  /**
+   * Batch Transforms given Names of Locations in objects containing lat/long
+   * @param array Strings of Location Names
+   * @return Promise On resloving JSON with objects
+   */
+  batchGeocode(array) {
+    
+  }
+};
+
+export default Geocoder;
