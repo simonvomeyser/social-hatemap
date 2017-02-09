914fc424c0914ec4f5cca70dbe12de1bf25d8b6f
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-09 10:05:29 +0100

    Geocoder - random Location function working

--- src/apis/geocoding/Geocoder.js
+++ src/apis/geocoding/Geocoder.js
@@ -4,19 +4,38 @@ const Geocoder = {
   /**
    * Batch Transforms given Names of Locations in objects containing lat/long
    * 
-   * @param array Strings of Location Names
-   * @return Promise On resloving JSON with objects
+   * @param {array} Strings of Location Names
+   * @return {Promise} On resloving JSON with objects
    */
   batchGeocode(locationNames =[]) {
+    throw new Error("Not yet implemented");
+  },
+
+  /**
+   * "Fake" development function to not call api during development
+   * @param  {Array}  locationNames Array of Strings representing locations
+   * @return {Prommise}               [description]
+   */
+  batchGeocodeStatic(locationNames =[]) {
     return locationNames.map((locationName) => {
-      return { locationName, ...getRandomGeoLoacation() };
+      return { locationName, ...this.getRandomGeoLoacation() };
     });
   },
 
+  /**
+   * @return {Object} With random long and lat prop
+   */
   getRandomGeoLoacation() {
-    const long = Math.random() * Math.PI * 2;
-    const lat = Math.acos(Math.random() * 2 - 1);
-    return [long, lat];
+    const long = Math.random() * 180 * this.randomNegative();
+    const lat = Math.random() * 90 * this.randomNegative();
+    return {long, lat};
+  },
+
+  /**
+   * @return -1 or 1
+   */
+  randomNegative() {
+    return Math.random() < 0.5 ? -1 : 1;
   }
 };
 
