507aa71ddf2b5687530c201b6a3cdc9205fb3b14
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-09 09:11:56 +0100

    geocaoder funciton mockup

--- src/apis/geocoding/Geocoder.js
+++ src/apis/geocoding/Geocoder.js
@@ -3,11 +3,20 @@ const apiUrl = 'http://api.socialhatemap.com/index.php';
 const Geocoder = {
   /**
    * Batch Transforms given Names of Locations in objects containing lat/long
+   * 
    * @param array Strings of Location Names
    * @return Promise On resloving JSON with objects
    */
-  batchGeocode(array) {
-    
+  batchGeocode(locationNames =[]) {
+    return locationNames.map((locationName) => {
+      return { locationName, ...getRandomGeoLoacation() };
+    });
+  },
+
+  getRandomGeoLoacation() {
+    const long = Math.random() * Math.PI * 2;
+    const lat = Math.acos(Math.random() * 2 - 1);
+    return [long, lat];
   }
 };
 
