41c09cfc4715ecb00ec2b2ec48133ce0185e68c0
Simon vom Eyser <simon.vom.eyser@gmail.com>
2017-02-12 19:56:25 +0100

    Render GridElements on after another with delay (for nice animation)

--- src/components/MapGrid/MapGrid.js
+++ src/components/MapGrid/MapGrid.js
@@ -8,22 +8,52 @@ import './MapGrid.css';
  * The Map on which the tweets are shown
  */
 class MapGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridElements : [],
+      areAllElementsRendered: false
+    };
+  }
+  componentDidMount() {
+    this.renderTiles();
+  }
   render() {
     return (
       <div className="MapGrid">
-        {this.renderTiles()}    
+        {this.state.gridElements.map((e) => e)}   
       </div>
     );
   }
   /**
+   * Complicated way to render tiles "one after another"
    * @return {jsx} 
    */
   renderTiles() {
     const tileWidth = 100 / this.props.tilesInRow;
     const gridElements = this.createGridElementArray();
 
-    return gridElements.map((e) => {
-      return (<MapGridElement key={"MapGridElement"+e.row+e.col} {...e} tileWidth={tileWidth} />);
+    gridElements.forEach((e, i) => {
+      // Increases each cycle
+      const delay = (10+i/2)*i; 
+
+      // Add GirdElements to state piece by piece with delay
+      setTimeout(() => {
+        const newGridElements = [...this.state.gridElements];
+
+        newGridElements.push(
+          <MapGridElement 
+            key={"MapGridElement"+e.row+e.col} 
+            row={e.row}
+            col={e.col}
+            tileWidth={tileWidth} />
+        );
+
+        this.setState({
+          gridElements: newGridElements,
+          areAllElementsRendered: i === gridElements.lenght-1
+        });
+      }, delay);
     });
   }
 
@@ -32,11 +62,11 @@ class MapGrid extends React.Component {
    * @return {Array} Of Objects with row/col number
    */
   createGridElementArray() {
-    const rowCount = Math.ceil(window.innerHeight / (window.innerWidth / this.props.tilesInRow));
+    const rowCount = Math.floor(window.innerHeight / (window.innerWidth / this.props.tilesInRow));
     const gridElements = [];
 
-    for (var row = 0; row < rowCount; row++) {
-      for (var col = 0 ; col < this.props.tilesInRow ; col++) {
+    for (var col = 0 ; col < this.props.tilesInRow ; col++) {
+      for (var row = 0; row < rowCount; row++) {
         gridElements.push({row, col});
       }
     }
