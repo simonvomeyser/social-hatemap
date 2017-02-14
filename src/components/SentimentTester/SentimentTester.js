b15cbc4d84a3857936108f4b415b8e3876198f7f
Felix Jordan <felix.jordan32@yahoo.de>
2017-02-14 13:55:13 +0100

    add tweet to sentiment site

--- src/components/SentimentTester/SentimentTester.js
+++ src/components/SentimentTester/SentimentTester.js
@@ -4,6 +4,8 @@ import './SentimentTester.css';
 
 import Sentiment from '../../helpers/Sentiment';
 
+import sampleData from '../../apis/twitter/sampleData.json';
+
 import $ from 'jquery';
 
 /**
@@ -25,15 +27,19 @@ export default class SentimentView extends React.Component {
 
     this.renderSentimentText     = this.renderSentimentText.bind(this);
   }
-  renderSentimentText(e){
-    e.preventDefault();
-    var sentence = $('#sentence').val();
-    var sentimentData;
-    if(sentence == "")
-      sentimentData = Sentiment.getSentiment("Hello Fucking WORLD");
-    else
-      sentimentData = Sentiment.getSentiment(sentence);
-    this.setState({sentimatedSentence: sentence });
+
+  componentWillMount() {
+    this.getTweet();
+  }
+
+  getTweet() {
+    var sentence = sampleData[[Math.floor(Math.random()*sampleData.length)]].text;
+    var sentimentData = Sentiment.getSentiment(sentence);
+    this.changeStates(sentence, sentimentData);
+  }
+
+  changeStates(sentence, sentimentData){
+    this.setState({sentimatedSentence: sentence })
     this.setState({amplitude: sentimentData.amplitude });
     this.setState({dirtiness: sentimentData.dirtiness });
     this.setState({label: sentimentData.label });
@@ -42,7 +48,16 @@ export default class SentimentView extends React.Component {
     this.setState({sentiment: sentimentData.sentiment });
   }
 
-
+  renderSentimentText(e){
+    e.preventDefault();
+    var sentimentData;
+    var sentence = $('#sentence').val();
+    if(sentence == "")
+      sentimentData = Sentiment.getSentiment("Hello Fucking WORLD");
+    else
+      sentimentData = Sentiment.getSentiment(sentence);
+    this.changeStates(sentence, sentimentData);
+  }
 
   render() {
     return (
