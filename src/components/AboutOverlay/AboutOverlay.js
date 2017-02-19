55f30a9c88ef2d6d06a6f645e3fdae3d9aed5e62
Marc Kloubert <mail@marckloubert.com>
2017-02-19 13:49:36 +0100

    about-overlay v1

--- src/components/AboutOverlay/AboutOverlay.js
+++ src/components/AboutOverlay/AboutOverlay.js
@@ -12,17 +12,36 @@ export default class AboutOverlay extends React.Component {
   render() {
     return (
       <div className="AboutOverlay">
-        {this.props.close ? <div onClick={this.props.close} className="AboutOverlay__close">&times;</div> : null }
-        <div class="wrapper">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-sm-12">
-                <h1>About Social Map</h1>
+        <div className="wrapper">
+          <div className="container-fluid">
+            <div className="row topSpacing1 first">
+              <div className="col-sm-11">
+                <h2 className="pageTitle">About Social Hate Map</h2>
+              </div>
+              <div className="col-sm-1 textRight">
+                {this.props.close ? <div onClick={this.props.close} className="AboutOverlay__close">&times;</div> : null }
               </div>
             </div>
-            <div class="row">
-              <div class="col-sm-12">
-
+            <div className="row topSpacing1">
+              <div className="col-sm-7">
+                <h3>Concept</h3>
+                <p>
+                  The Social Hate Map visualizes properties of social media messages and users. The data is pulled over APIs, evaluated and then mapped on a global map.
+                </p>
+                <p> 
+                  Key visualization elements are small monsters, their respective colors and shapes representing different properties of the tweets. Special focus is set on the sentiment of the messages (sentiment analysis) which is displayed in the overall angrieness level of the monsters, in a way that sentiments regarding a topic can quickly be seized. 
+                </p>
+              </div>
+            </div>
+            <div className="row topSpacing2">
+              <div className="col-sm-7">
+                <h3>Team</h3>
+                <p>
+                  <a href="http://simonvomeyser.de/" title="Simon vom Eyser">Simon vom Eyser</a>, 582460<br/>
+                  Felix Jordan, XXXXXX<br/>
+                  Stefan "Boerdi" Weber, XXXXXX<br/>
+                  <a href="http://marckloubert.com" title="Marc Kloubert">Marc Kloubert</a>, 567230
+                </p>
               </div>
             </div>
           </div>
